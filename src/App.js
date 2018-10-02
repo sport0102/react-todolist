f40f53720143842db17194ece015fe649896e247
이선민 <smlee2@comtrue.com>
2018-10-02 17:07:59 +0900

    Add input action, todo list

--- src/App.js
+++ src/App.js
@@ -4,10 +4,61 @@ import Form from './components/Form'
 import TodoItemList from './components/TodoItemList';
 
 class App extends Component {
+  id = 3 // 이미 0,1,2 가 존재하므로 3으로 설정
+
+  state = {
+    input: '',
+    todos: [
+      { id: 0, text: ' 리액트 소개', checked: false },
+      { id: 1, text: ' 리액트 소개', checked: true },
+      { id: 2, text: ' 리액트 소개', checked: false }
+    ]
+  }
+handleChange=(e)=>{
+  this.setState({
+    input:e.target.value //input의 다음 바뀔 값
+  })
+}
+
+handleCreate=()=>{
+  const{input,todos}=this.state
+  this.setState({
+    input:'',
+    todos:todos.concat({
+      id:this.id++,
+      text:input,
+      checked:false
+    })
+  })
+}
+
+
+handleKeyPress = (e) => {
+  // 눌려진 키가 Enter 면 handleCreate 호출
+  if(e.key === 'Enter') {
+    this.handleCreate();
+  }
+}
+
+
   render() {
+    const{input,todos}=this.state
+    const{
+      handleChange,
+      handleCreate,
+      handleKeyPress
+    }=this
+
     return (
-     <TodoListTemplate form={<Form/>}>
-       <TodoItemList/>
+     <TodoListTemplate form={(
+     <Form
+      value={input}
+      onKeyPress={handleKeyPress}
+      onChange={handleChange}
+      onCreate={handleCreate}
+     />
+     )}>
+       <TodoItemList todos={todos}/>
      </TodoListTemplate>
     );
   }
